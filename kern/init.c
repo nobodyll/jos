06356154e7bf01c21ee7c82dba02e5f0b79cdf21
wyl <kofjoker0@gmail.com>
2023-11-05 13:39:27 +0800

    commit lab1

--- kern/init.c
+++ kern/init.c
@@ -35,7 +35,11 @@ i386_init(void)
 
 	cprintf("6828 decimal is %o octal!\n", 6828);
 
-	// Test the stack backtrace function (lab 1 only)
+	// int x = 1, y = 3, z = 4;
+	// cprintf("x %d, y %x, z %d\n", x, y, z);
+	// cprintf("x=%d y=%d\n", 3);
+
+  // Test the stack backtrace function (lab 1 only)
 	test_backtrace(5);
 
 	// Drop into the kernel monitor.
