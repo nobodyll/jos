2470c55b4b44a3d22663cacb2525329d9a2302fb
wyl <kofjoker0@gmail.com>
2023-12-28 21:46:28 +0800

    commit exercise 3

--- kern/pmap.c
+++ kern/pmap.c
@@ -313,7 +313,10 @@ mem_init_mp(void)
 	//     Permissions: kernel RW, user NONE
 	//
 	// LAB 4: Your code here:
-
+	for (int i = 0; i < NCPU; i++) {
+		uint32_t kstacktop = KSTACKTOP - i * (KSTKSIZE + KSTKGAP);
+		boot_map_region(kern_pgdir, kstacktop - KSTKSIZE, KSTKSIZE, PADDR(percpu_kstacks[i]), PTE_W);
+	}
 }
 
 // --------------------------------------------------------------
